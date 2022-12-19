d9043bec1d60cb1328a5841978b0c77bc249c333
Nate Ensign <dixienate@gmail.com>
2022-12-18 20:23:49 -0700

    complete through project part 1

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import potentialCountriesReducer from "../redux/slices/potentialCountriesSlice"
+
+export default configureStore({
+    reducer: { potentialCountries: potentialCountriesReducer},
+});
+
